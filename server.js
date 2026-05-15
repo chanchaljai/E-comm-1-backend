0e11af44c745d4ba1aadd308ce15ffa1332c86e0
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-15 11:06:53 +0530

    db server

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+const app = express();
+express();
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("API Running"));
+
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
