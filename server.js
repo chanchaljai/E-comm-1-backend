750a5338e2cc25701108b78a26b4b2a7759058da
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-16 18:10:56 +0530

    thunder client register/login test ok

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use("/api/auth", router);
 
 
+
 app.get("/", (req, res) => res.send("API Running"));
 
 app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
